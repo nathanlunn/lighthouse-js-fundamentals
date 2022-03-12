f0415a9cc10670b698d9b4f89ca73a0dd1ceb742
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-03-12 13:09:51 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,16 +1,35 @@
-const raining = true;
-const temp = 12;
+// const raining = true;
+// const temp = 12;
 
-if (raining) {
-  console.log("Don't forget to bring an umbrella!");
+// if (raining) {
+//   console.log("Don't forget to bring an umbrella!");
+// }
+
+// if (temp < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else  if (temp < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
 
-if (temp < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else  if (temp < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+
+if (temp < -40 || temp > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
 }
 
-console.log("Now you're ready to go outside!");
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
